d596b592874cfa7302dc627cdc0f45f3c93c3a88
Samuele Resca <samuele.resca@gmail.com>
2016-01-04 23:09:28 +0100

    Parametrized URLs

--- BlogPublishersDemo/BlogPublishersDemo/urls.py
+++ BlogPublishersDemo/BlogPublishersDemo/urls.py
@@ -9,5 +9,5 @@ from app.forms import BootstrapAuthenticationForm
 urlpatterns = patterns('',
     url(r'^$', 'app.views.home', name='home'),
     url(r'^books$', 'app.views.books', name='books'),
-    url(r'^publishers', 'app.views.publishers', name='publishers'),
-)
+    url(r'^books/(?P<id>\d+)/$', 'app.views.books_detail', name='books'),
+    url(r'^publishers', 'app.views.publishers', name='publishers'),)
--- BlogPublishersDemo/app/views.py
+++ BlogPublishersDemo/app/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render
 from django.http import HttpRequest
 from django.template import RequestContext
 from datetime import datetime
+from app.models import *
 
 def home(request):
     """Renders the home page."""
@@ -29,6 +30,18 @@ def books(request):
             'message':'Your books page.',
             'year':datetime.now().year,
         }))
+
+def books_detail(request, id):
+    """Renders the books page."""
+    assert isinstance(request, HttpRequest)
+
+    current_book = Book.objects.filter(id=id).first()
+
+
+    return render(request,
+        'app/books-detail.html',
+        context_instance = RequestContext(request, {
+            "book": current_book }))
 
 def publishers(request):
     """Renders the publishers page."""
